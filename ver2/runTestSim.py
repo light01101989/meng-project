0143cb49b81649c02308514ab62854d892e7c512
Arjun Jauhari <arjunthought@gmail.com>
2016-04-15 01:50:17 -0400

    Major: User parameter PHI is now incorporated into the model and new function added for this new objective function and its gradient + some minor changes

--- ver2/runTestSim.py
+++ ver2/runTestSim.py
@@ -1,5 +1,17 @@
 import simulation
 import visualize
+import numpy as np
+import matplotlib.pyplot as plt
 
-simulation.runTest()
-visualize.testVisualize()
+N=200
+kTau = np.zeros(N)
+for i in xrange(N):
+    print i
+    kTau[i] = simulation.runTestUser(i+1)
+plt.plot(range(1, N+1),kTau, 'b')
+plt.title('KendallTau')
+plt.xlabel('Number of clicks(N)')
+plt.savefig('KTau-User.png')
+plt.show()
+#simulation.runTest()
+#visualize.testVisualize()
--- ver2/simulation.py
+++ ver2/simulation.py
@@ -107,6 +107,102 @@ def adaGrad(initialGuess, voteHist):
     print "numIteration: ",cnt
     return out
 
+##########################################################################
+## Objective function with User parameter also included                 ##
+##########################################################################
+def genHist(N, cDist):
+    np.random.seed(110)
+    vote = np.array([])
+    # Generate a bunch of clickers(users choices)
+    for i in range(N):
+        cvote = np.random.rand(1)
+        vote = np.append(vote,cvote)
+        dvote = np.digitize(vote, cDist)
+        #voteHist = [[v,np.ones_like(Theta)] for v in dvote]
+        voteHist = [[v,list(xrange(cDist.size))] for v in dvote]
+
+    return voteHist
+
+def objFuncUser(thetaVec, *args):
+    # get from args
+    uaidx = args[0]
+    qaidx = args[1]
+    uphiidx = args[2]
+    qvHist = args[3]
+
+    grad = np.zeros_like(thetaVec)
+
+    # get term 1: P(clicks|theta)
+    t1 = 0
+    for key in qvHist:
+        t1o, t1g = term1(thetaVec[qaidx[key]], qvHist[key])
+        #t1 += term1(thetaVec[qaidx[key]], qvHist[key])
+        t1 += t1o
+        grad[qaidx[key]] += t1g
+
+    # get term 2: P(theta|Phi)
+    # get term 3: P(Phi)
+    # FIXME: Does sigma/constant matter?
+    t2 = 0
+    t3 = 0
+    for key in uaidx:
+        t2 += np.sum(np.square(thetaVec[uaidx[key]] - thetaVec[uphiidx[key]]))
+        grad[uaidx[key]] += 2*(thetaVec[uaidx[key]] - thetaVec[uphiidx[key]])
+        grad[uphiidx[key]] += np.sum(-2*(thetaVec[uaidx[key]] - thetaVec[uphiidx[key]]))
+        t3 += np.square(thetaVec[uphiidx[key]])
+        grad[uphiidx[key]] += 2*thetaVec[uphiidx[key]]
+
+    # Final output
+    out = t1+t2+t3
+    return out,grad
+    #return out
+
+# Just for check grad
+def objFuncUserGrad(thetaVec, *args):
+    # get from args
+    uaidx = args[0]
+    qaidx = args[1]
+    uphiidx = args[2]
+    qvHist = args[3]
+
+    grad = np.zeros_like(thetaVec)
+
+    # get term 1: P(clicks|theta)
+    t1 = 0
+    #t1grad = 0
+    for key in qvHist:
+        t1o, t1g = term1(thetaVec[qaidx[key]], qvHist[key])
+        #t1 += term1(thetaVec[qaidx[key]], qvHist[key])
+        t1 += t1o
+        grad[qaidx[key]] += t1g
+
+    # get term 2: P(theta|Phi)
+    # get term 3: P(Phi)
+    # FIXME: Does sigma/constant matter?
+    t2 = 0
+    t3 = 0
+    for key in uaidx:
+        t2 += np.sum(np.square(thetaVec[uaidx[key]] - thetaVec[uphiidx[key]]))
+        grad[uaidx[key]] += 2*(thetaVec[uaidx[key]] - thetaVec[uphiidx[key]])
+        grad[uphiidx[key]] += np.sum(-2*(thetaVec[uaidx[key]] - thetaVec[uphiidx[key]]))
+        t3 += np.square(thetaVec[uphiidx[key]])
+        grad[uphiidx[key]] += 2*thetaVec[uphiidx[key]]
+
+    # Final output
+    #print t1+t2+t3
+    out = t1+t2+t3
+    return grad
+
+def term1(thetaVec, *args):
+    voteHist = args[0]
+    out = 0
+    grad = np.zeros_like(thetaVec)
+    for i in range(len(voteHist)):
+        out += objFunc2Core(thetaVec, voteHist[i])
+        grad += objFunc2CoreGrad(thetaVec, voteHist[i])
+
+    return out,grad
+
 # Simulate N new clicks from every iteration
 def simulateClicks(N, cDist):
     # Generate a bunch of clickers(users choices)
@@ -115,7 +211,7 @@ def simulateClicks(N, cDist):
     voteCount = np.histogram(vote, bins=cDist.size, range=(0,cDist.size))[0]
     #print "Votes for each answer:"
     #print voteCount
-    
+
     # Optimization
     initialVal = np.ones(10)*0.1
     #initialVal = np.random.randn(10)
@@ -136,7 +232,7 @@ def simulateClicks2(N, cDist, Theta):
         voteCount = np.histogram(dvote, bins=cDist.size, range=(0,cDist.size))[0]
         #print "Votes for each answer:"
         #print voteCount
-        
+
         # Optimization
         initialVal = np.ones(10)*0.1
         #initialVal = np.random.randn(10)
@@ -181,7 +277,7 @@ def runTest():
     # Generate sample Theta's which tells the relative goodness of answers
     # Suppose there are 10 answers
     Theta = np.random.randn(10)
-    
+
     # pDensity
     pDens = np.exp(Theta)/np.sum(np.exp(Theta))
     cDist = np.cumsum(pDens)
@@ -194,13 +290,13 @@ def runTest():
     print "Close the figure to run simulation"
     plt.savefig('PDF-sd%d.png'%sd)
     plt.show()
-    
+
     N = 200
     kTau = np.zeros(N)
     for i in range(N):
         ThetaEst = simulateClicks(i+1, cDist)
         kTau[i] = kendalltau(Theta, ThetaEst)[0]
-    
+
     kTau2 = simulateClicks2(N, cDist, Theta)
     kTau3 = simClicksTime(N, cDist, Theta)
     plt.plot(range(1, N+1),kTau, 'b')
@@ -210,3 +306,78 @@ def runTest():
     plt.xlabel('Number of clicks(N)')
     plt.savefig('KTau-sd%d.png'%sd)
     plt.show()
+
+def runTestUser(N):
+    sd = 110
+    np.random.seed(sd)
+    # Generate sample Phi's which tells the relative goodness of users
+    # Suppose there are 10 users
+    nUser = 10
+    Phi = np.random.randn(nUser)
+
+    # convert it to probability density function
+    # pDensity
+    pDens = np.exp(Phi)/np.sum(np.exp(Phi))
+    cDist = np.cumsum(pDens)
+    #aNum = np.ones(nUser)
+    #aNum[0] = 0
+    #aNum = np.cumsum(aNum)
+    #plt.bar(aNum, pDens)
+    #plt.plot(aNum, cDist)
+    #plt.title('PDensityF/CDistributionF')
+    #print "Close the figure to run simulation"
+    #plt.savefig('PDF-sd%d.png'%sd)
+    #plt.show()
+
+    # Generate sample Theta's which tells the relative goodness of answers
+    # Suppose every user has generated 10 answers
+    numAnsPuser = 10
+    Theta = np.zeros((nUser,numAnsPuser))
+    for i in range(nUser):
+        Theta[i,:] = np.random.randn(numAnsPuser)
+
+    Theta = Theta + Phi[np.newaxis].T
+
+    #N = 200     # num of clicks
+
+    # Prepare data structure to pass to objFuncUser
+    totalAns = numAnsPuser*nUser
+    qaidx = {}
+    uaidx = {}
+    qvHist = {}
+    uphiidx = {}
+    numQues = 10
+    numAnsPques = 10
+    #pdb.set_trace()
+    for i in xrange(numQues):
+        qaidx[i] = []
+        qvHist[i] = []
+
+    for i in xrange(nUser):
+        uaidx[i] = []
+        uphiidx[i] = totalAns+i
+
+    for i in xrange(totalAns):
+        qaidx[np.floor(i/numAnsPques)].append(i)
+        uaidx[i%numAnsPques].append(i)
+
+    #print(qaidx)
+    #print(uaidx)
+
+    # generate clicks for each ques
+    for i in xrange(numQues):
+        pDens = np.exp(Theta[:,i])/np.sum(np.exp(Theta[:,i]))
+        cDist = np.cumsum(pDens)
+        qvHist[i] = genHist(N, cDist)
+
+    # optimize
+    initialVal = np.ones(totalAns+nUser)*0.1
+    #print "Error with User:"
+    #print optimize.check_grad(objFuncUser,objFuncUserGrad,np.random.randn(totalAns+nUser),uaidx, qaidx, uphiidx, qvHist)
+    #sol, f, d = optimize.fmin_l_bfgs_b(objFuncUser, initialVal, approx_grad=1, args=(uaidx, qaidx, uphiidx, qvHist), disp=1)
+    sol, f, d = optimize.fmin_l_bfgs_b(objFuncUser, initialVal, args=(uaidx, qaidx, uphiidx, qvHist), disp=0)
+
+    #print Phi
+    #print sol
+    #print kendalltau(Phi,sol[100:110])
+    return kendalltau(Phi,sol[100:110])[0]
